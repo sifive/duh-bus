c836e0523a875ff250eb9efc7817d4339f1abdd1
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-06-21 17:24:24 -0700

    added indexer for bundled index

---
+++ bin/indexer.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+'use strict';
+
+const path = require('path');
+const fs = require('fs-extra');
+const json5 = require('json5');
+
+function walker (cb, root) {
+  return function rec (dir) {
+    fs.readdir(dir).then(subdirs => {
+      Promise.all(subdirs.map(subdir => {
+        const res = path.resolve(dir, subdir);
+        fs.stat(res).then(e => {
+          if (e.isDirectory()) {
+            rec(res);
+          } else {
+            cb(path.relative(root, res), res);
+          }
+        });
+      }));
+    });
+  };
+}
+
+function exporter (path) {
+  const arr = path.split('/');
+  const res = 'xassign([' + arr.map(e => `'${e}'`).join(', ') + '])(';
+  return res;
+}
+
+function main () {
+  console.log(`'use strict';
+
+const xassign = path => obj =>
+  Object.assign(path.reduce((res, cur) =>
+    res[cur] || (res[cur] = {}), exports), obj);
+`);
+  const root = path.resolve(process.cwd(), 'specs');
+  fs.pathExists(root).then(exists => {
+    if (exists) {
+      walker((short, full) => {
+        fs.readFile(full, 'utf8').then(body => {
+          json5.parse(body);
+          console.log(exporter(short) + body + ');\n');
+        });
+      }, root)(root);
+    }
+  });
+}
+
+main();
