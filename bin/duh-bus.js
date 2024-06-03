2e021f81436e4fdf5a5c5bb9453b56c467865d6e
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2024-06-02 17:30:47 -0700

    integrated Downlod CLI option

--- bin/duh-bus.js
+++ bin/duh-bus.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 'use strict';
 
+const process = require('process');
 const path = require('path');
+const { program } = require('commander');
+const dl = require('../lib/amba-pdf-dl.js');
 
-const specPath = path.resolve(__dirname, '../specs');
+const main = async () => {
+  program
+    .option('--amba-pdf-dl <folder>', 'download AMBA spec PDFs')
+    .parse(process.argv);
 
-console.log(specPath);
+  const opts = program.opts();
+  if (opts.ambaPdfDl) {
+    await dl(opts.ambaPdfDl);
+    return;
+  }
+  const specPath = path.resolve(__dirname, '../specs');
+  console.log(specPath);
+};
+
+main();
+
 /* eslint no-console:0 */
